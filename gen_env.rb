561cab9ca3c3bd886ba5d614035d50a45978a8b2
johnlinvc <johnlinvc@gmail.com>
2017-02-15 23:56:30 +0800

    check if it's pr for jenkins

--- gen_env.rb
+++ gen_env.rb
@@ -2,14 +2,16 @@
 ci = ENV['CI_TYPE']
 case ci
 when "TRAVIS"
-  keys = %W{HAS_JOSH_K_SEAL_OF_APPROVAL TRAVIS_PULL_REQUEST TRAVIS_REPO_SLUG TRAVIS_PULL_REQUEST DANGER_GITHUB_API_TOKEN}
+  keys = %W{HAS_JOSH_K_SEAL_OF_APPROVAL TRAVIS_REPO_SLUG TRAVIS_PULL_REQUEST}
   env = keys.map{|k| [k,ENV[k]]}.to_h
 when "JENKINS"
+  pr_id = ENV["TRAVIS_PULL_REQUEST"].to_i > 0 ? ENV["TRAVIS_PULL_REQUEST"] : ""
   env = {
     JENKINS_URL: "http://localhost",
     GIT_URL: "https://github.com/johnlinvc/danger-docker",
-    ghprbPullId: ENV["TRAVIS_PULL_REQUEST"],
-    DANGER_GITHUB_API_TOKEN: ENV["DANGER_GITHUB_API_TOKEN"]
+    ghprbPullId: pr_id
   }
 end
+
+env[:DANGER_GITHUB_API_TOKEN]=ENV["DANGER_GITHUB_API_TOKEN"]
 env.each{|k,v| puts "#{k}=#{v}"}
